90226d8abe898c1cc3deb82f24ae5474923bbd31
dino <dino.0201.5859@gmail.com>
2022-12-10 17:04:55 +0800

    test5

---
+++ SecondServlet/src/main/java/EEIT56_23/ShowPeopleController2.java
@@ -0,0 +1,31 @@
+package EEIT56_23;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/showPeopleControllersafe2")
+public class ShowPeopleController2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	//用ID搜尋
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Integer id = Integer.valueOf(request.getParameter("id"));
+		PeopleService_23 peopleService = new PeopleServiceImpl_23();
+		PeopleBean_23 bean = peopleService.showById(id);
+		
+		request.setAttribute("bean", bean);
+		request.getRequestDispatcher("/EEIT56_23/ShowUserSelectForm.jsp").forward(request, response);
+	}
+
+}

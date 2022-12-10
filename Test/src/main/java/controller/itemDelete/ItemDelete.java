f595608d4f78a86e8592c11e0f8698ec94f4f47e
015 <aaa@>
2022-12-10 15:47:16 +0800

    test

---
+++ Test/src/main/java/controller/itemDelete/ItemDelete.java
@@ -0,0 +1,35 @@
+package controller.itemDelete;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ItemDao;
+import dao.ItemDaoImpl;
+import model.Item;
+
+@WebServlet("/ItemDelete.do")
+public class ItemDelete extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ItemDao iDao = new ItemDaoImpl();
+		iDao.deleteById(Integer.parseInt(request.getParameter("id")));
+		RequestDispatcher rd = request.getRequestDispatcher("/ItemList/ItemDelete.jsp");
+		rd.forward(request, response);
+		return;
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Test/src/main/java/util/Init.java
@@ -0,0 +1,30 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import util.DBservice;
+
+public class Init {
+
+	public static void main(String[] args) throws SQLException {
+		try (
+				Connection con = DriverManager.getConnection(
+							DBservice.getDbUrl(), 
+							DBservice.getUser(),
+							DBservice.getPassword()); 
+				Statement stmt = con.createStatement();
+			){
+			stmt.executeUpdate(DBservice.getCreateItems());
+			stmt.executeUpdate(DBservice.getCreateOrders());
+			stmt.executeUpdate(DBservice.getCreateOrderItems());
+			
+			//System.out.println("dddddddddddddddddd");
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
